ebb66dc19d9be8eefd546dcd4e3bca2678dfc4a1
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2025-03-29 12:26:17 +0100

    Update to ESLint 9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import jest from "eslint-plugin-jest";
+import stylistic from "@stylistic/eslint-plugin";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import github from "eslint-plugin-github";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default defineConfig([
     globalIgnores(["**/dist/", "**/lib/", "**/node_modules/", "**/jest.config.js"]),
-    {
-        extends: compat.extends("plugin:github/recommended"),
 
+    ...github.getFlatConfigs().typescript,
+    github.getFlatConfigs().recommended,
+
+    {
         plugins: {
             jest,
+            stylistic,
             "@typescript-eslint": typescriptEslint,
         },
 
@@ -33,7 +32,7 @@ export default defineConfig([
             },
 
             parser: tsParser,
-            ecmaVersion: 9,
+            ecmaVersion: "latest",
             sourceType: "module",
 
             parserOptions: {
@@ -46,24 +45,24 @@ export default defineConfig([
             "eslint-comments/no-use": "off",
             "import/no-namespace": "off",
             "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "error",
+            "func-call-spacing": ["error", "never"],
+            "semi": ["error", "never"],
 
+            "stylistic/type-annotation-spacing": "error",
+
+            "@typescript-eslint/no-unused-vars": "error",
             "@typescript-eslint/explicit-member-accessibility": ["error", {
                 accessibility: "no-public",
             }],
-
             "@typescript-eslint/no-require-imports": "error",
             "@typescript-eslint/array-type": "error",
             "@typescript-eslint/await-thenable": "error",
             "@typescript-eslint/ban-ts-comment": "error",
             camelcase: "off",
             "@typescript-eslint/consistent-type-assertions": "error",
-
             "@typescript-eslint/explicit-function-return-type": ["error", {
                 allowExpressions: true,
             }],
-
-            "@typescript-eslint/func-call-spacing": ["error", "never"],
             "@typescript-eslint/no-array-constructor": "error",
             "@typescript-eslint/no-empty-interface": "error",
             "@typescript-eslint/no-explicit-any": "error",
@@ -84,9 +83,6 @@ export default defineConfig([
             "@typescript-eslint/promise-function-async": "error",
             "@typescript-eslint/require-array-sort-compare": "error",
             "@typescript-eslint/restrict-plus-operands": "error",
-            semi: "off",
-            "@typescript-eslint/semi": ["error", "never"],
-            "@typescript-eslint/type-annotation-spacing": "error",
             "@typescript-eslint/unbound-method": "error",
         },
     },
